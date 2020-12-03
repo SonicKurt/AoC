47846533deb1840af6122094462f940c15709095
Kurt Campbell <44346740+SonicKurt@users.noreply.github.com>
2020-12-03 14:05:05 -0500

    created part 1 func

--- Day3/day3.cpp
+++ Day3/day3.cpp
@@ -2,12 +2,13 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <cassert>
 
 using namespace std;
 
 vector<string> mapInput(string filename) {
     ifstream fileInput;
-    fileInput.open("input.txt");
+    fileInput.open(filename);
 
     vector<string> map;
 
@@ -27,9 +28,31 @@ vector<string> mapInput(string filename) {
     return map;
 }
 
+int treeCount(vector<string> &map, int x, int y) {
+    int result = 0;
+    int posX, posY;
+    for (auto line : map) {
+        posX = (x + posX) % line.length();
+        posY = y + posY;
 
+        if (posY >= map.size()) {
+            return result;
+        }
+        if (map[posY][posX] == '#') {
+            result++;
+        }
+    }
+
+    return result;
+}
 
 int main() {
+    auto exampleMap = mapInput("example.txt");
+    auto map = mapInput("input.txt");
+
+    assert(treeCount(exampleMap, 3, 1) == 7);
+    cout << treeCount(map, 3, 1) << endl;
+    
 
     return 0;
 }
